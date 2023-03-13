9f6a57286fbeae650bcddfa87a29ad288f83a2f8
Luka Robajac <luka.robajac@gmail.com>
2023-03-13 18:59:00 +0100

    Add product routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 //Route::prefix('/clients')->group(function() {
@@ -18,3 +19,10 @@ Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('c
 Route::post('/clients/store', [ClientController::class, 'store'])->name('clients.store');
 Route::put('/clients/{client}/update', [ClientController::class, 'update'])->name('clients.update');
 Route::delete('/clients/{client}/delete', [ClientController::class, 'destroy'])->name('clients.destroy');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+Route::put('/products/{product}/update', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}/delete', [ProductController::class, 'destroy'])->name('products.destroy');
